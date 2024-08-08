5daeedb918a166f806e7913021a87caa0b75b761
HUAN WANG <wanghuan_hh@126.com>
2024-08-08 22:10:54 +0800

    added criteria based on RELAX_DETAIL.

--- check_PWmat_convergence.py
+++ check_PWmat_convergence.py
@@ -11,6 +11,12 @@ parser = ap.ArgumentParser(add_help=True,
                            Email:    huan.wang@whut.edu.cn,
                            Version:  v1.0,
                            Date:     August 7, 2024""")
+parser.add_argument("-i",
+                    metavar="<etot.input file>",
+                    type=Path,
+                    help="you can specify the file name",
+                    default=(Path.cwd() / "etot.input"),
+                    )
 parser.add_argument("-r",
                     metavar="<RELAXSTEPS file>",
                     type=Path,
@@ -30,25 +36,51 @@ parser.add_argument("-v", "--verbose",
 args = parser.parse_args()
 
 
-def read_relaxsteps(filename):
+def read_etot_input(filename):
+    pattern = r"RELAX_DETAIL = (.*)"
+    with open(filename, "r") as fo:
+        for line in fo:
+            m = re.findall(pattern, line)
+            if m:
+                info = m[0].split()
+                break
+    return len(info)
+
+
+def read_relaxsteps(filename, criteria):
     all_data = []
     with open(filename) as fo:
         for line in fo:
             info = line.strip().split()
-            all_data.append((info[1],    # iteration steps
-                             info[2],    # opt status
-                             info[4],    # Total Energy
-                             info[6],    # Average Force
-                             info[8],    # Maximum Force
-                             info[10],   # Average stress (eV/Number_of_atom)
-                             info[12],   # Delta E_total
-                             info[14],   # Delta Rho
-                             info[16],   # SCF
-                             info[18],   # Delta |R - R(new_initial)|
-                             info[20],   # Delta AL
-                             info[22],   # p*F
-                             info[24],   # p*F0
-                             info[26]))  # Fch (check Force)
+            if criteria > 3:    # for cell optimization
+                all_data.append((info[1],    # iteration steps
+                                 info[2],    # opt status
+                                 info[4],    # Total Energy
+                                 info[6],    # Average Force
+                                 info[8],    # Maximum Force
+                                 info[10],   # Average stress (eV/Number_of_atom)
+                                 info[12],   # Delta E_total
+                                 info[14],   # Delta Rho
+                                 info[16],   # SCF
+                                 info[18],   # Delta |R - R(new_initial)|
+                                 info[20],   # Delta AL
+                                 info[22],   # p*F
+                                 info[24],   # p*F0
+                                 info[26]))  # Fch (check Force)
+
+            elif criteria <= 3:    # for ionic optimization
+                all_data.append((info[1],    # iteration steps
+                                 info[2],    # opt status
+                                 info[4],    # Total Energy
+                                 info[6],    # Average Force
+                                 info[8],    # Maximum Force
+                                 info[10],   # Delta E_total
+                                 info[12],   # Delta Rho
+                                 info[14],   # SCF
+                                 info[16],   # Delta |R - R(new_initial)|
+                                 info[18],   # p*F
+                                 info[20],   # p*F0
+                                 info[22]))  # Fch (check Force)
     return all_data
 
 
@@ -72,16 +104,18 @@ def plot_deltE_deltF(data):
     
     fig, (ax1,ax2) = plt.subplots(2,1, figsize=(12,8))
     ax1.plot(step, Etot, "k-") 
-    ax2.plot(step, aveF, "b-")
     ax1.set_xlim(step.min(), step.max())
     ax1.set_ylim(Etot.min(), Etot.max())
     ax1.set_xlabel("step")
     ax1.set_ylabel("total Energy (eV)")
+    
+    ax2.plot(step, aveF, "b-")
     ax2.set_xlim(step.min(), step.max())
     ax2.set_ylim(aveF.min(), aveF.max())
     ax2.set_xlabel("step")
     ax2.set_ylabel("average Force (eV/Angstrom)")
     plt.show()
+
 
 def main():
     """
@@ -89,7 +123,8 @@ def main():
     1) read data from RELAXSTEPS file. If the user specifid the filename, the given file will be read.
     2) plot the Energy and the average Force against relax steps. If the user activates the verbose mode, detailed information will be printed on the screen.
     """
-    
+
+    etot_input = args.i
     relaxsteps = args.r
     print(type(etot_input))
     print(type(relaxsteps))
@@ -102,21 +137,42 @@ def main():
         except FileNotFoundError as e:
             print(e)
             print("""Exiting the program...\n
-                  Please check file name, or you can specify the file name.\n""")
+                  Please check the file name, or you can specify the file name.\n""")
             exit(1)
-
+    
     title = "iter, status, E_tot, Aver_F_ion, Max_F_ion, Aver_F_ele, delt_E, delt_rho, SCF_cyc, delt_L, delt_AL, proj_F, proj_F0, F_check".strip().split(",")
     tfmt = "".join(("{:>4s}","{:^7s}","{:^21s}","{:>11s}"*5,"{:>8s}","{:>9s}","{:>11s}"*4))
     ifmt = "".join(("{:>3}","{:>7s}","{:>22}","{:>11}"*5,"{:>6}","{:>11}"*5))
+
+    ifmt_cell = "".join(("{:>3}","{:>7s}","{:>22}","{:>11}"*5,"{:>6}","{:>11}"*5))
+    ifmt_ion  = "".join(("{:>3}","{:>7s}","{:>22}","{:>11}"*4,"{:>6}","{:>11}"*4))
+
+    criteria = read_etot_input(etot_input)
     info = read_relaxsteps(relaxsteps)
-    
-    if args.verbose:
-        print(tfmt.format(*title_cell))
-        for item in info:
-            print(ifmt.format(*item))
-        plot_deltE_deltF(np.array(data).reshape(-1, 14))
+
+    if info[-1][1] == '*END':
+        print("The optimization job completed.")
     else:
-        plot_deltE_deltF(np.array(data).reshape(-1, 14))
+        print("The optimization job is not yet complete!")
+
+    if args.verbose:
+        if criteria > 3:
+            print(tfmt.format(*title_cell))
+            for item in info:
+                print(ifmt.format(*item))
+            plot_deltE_deltF(np.array(data).reshape(-1, 14))
+
+        elif criteria < 3:
+            print(ifmt_ion.format(*title_ion))
+            for item in info:
+                print(ifmt.format(*item))
+            plot_deltE_deltF(np.array(data).reshape(-1, 12))
+
+    else:
+        if criteria > 3:
+            plot_deltE_deltF(np.array(data).reshape(-1, 14))
+        elif criteria < 3:
+            plot_deltE_deltF(np.array(data).reshape(-1, 12))
 
 
 if __name__ == "__main__":
