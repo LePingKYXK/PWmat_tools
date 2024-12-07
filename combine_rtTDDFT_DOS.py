4228b915fad4365e05241fc594ed52d70d0fb9e6
HUAN WANG <wanghuan_hh@126.com>
2024-12-07 23:49:06 +0800

    Create combine_rtTDDFT_DOS.py

---
+++ combine_rtTDDFT_DOS.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def read_DOS_file():
+    return data
+
+
+def main():
+    data = read_DOS_file()
+
+
+
+if __name__ == '__main__':
+    main()
